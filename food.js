027a6da684c840f125c9a259485417c5ff38eea6
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-12-20 14:13:59 -0500

    created an instance of food to test that the properties would show up

--- food.js
+++ food.js
@@ -1,11 +1,17 @@
 // Create class below
 class Food {
-    constructor(name, daysToSpoil, fresh = true) {
-        this.name = name;
-        this.daysToSpoil = daysToSpoil
-        this.fresh = fresh
-    }
-    
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  } 
 }
+
+const pasta = new Food("Alfredo", 5, true)
+console.log(pasta)
+
+
+
+
 // Do not edit below this line
 module.exports = Food;
