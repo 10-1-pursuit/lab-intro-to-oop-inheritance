01329562a9846c9a87b0d5ea6bac35d9ca77fdc3
anickacodes <anickalewis@pursuit.org>
2023-12-20 13:11:53 -0500

    Tamagotchi attributes ✅

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,8 +1,24 @@
 // Create class below
-class Tamagotchi{
-    constructor(name, energy=9,full=8,mood=6,sick=false,rehomed=false) {
-        this.name=name
-    }
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood
+    this.sick = sick
+    this.rehomed = rehomed
+  }
 }
+
+const pablo = new Tamagotchi("Pablo");
+
+console.log(pablo);
 // Do not edit below this line
 module.exports = Tamagotchi;
