91194d3d1773d3c27cb3ec9fa98a8366c9c478a8
jameel914 <jameelibrahim107@gmail.com>
2023-12-20 18:34:03 -0500

    lab

--- food.js
+++ food.js
@@ -1,4 +1,34 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    (this.name = name), (this.daysToSpoil = daysToSpoil), (this.fresh = fresh);
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    if (this.daysToSpoil <= 0) {
+      console.log(`${this.name} has spoiled.`);
+    } else {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    }
+  }
+  aDayPasses() {
+    this.daysToSpoil -= 1;
+    this.isFresh();
+  }
+}
+const food1 = new Food("Milk chocolate", 3);
+food1.prepare();
+console.log(food1);
+console.log("********************");
+food1.isFresh();
+console.log(food1);
+console.log("********************");
+food1.aDayPasses();
+console.log(food1);
 
 // Do not edit below this line
 module.exports = Food;
