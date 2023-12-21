db5590288754ec4447dd031972b49d35f282edae
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-12-21 11:50:04 -0500

    created a Tamagotchi class, gave it some default values, and a greet method

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,29 @@
 // Create class below
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {
+    console.log(`Hello my name is ${this.name}`);
+  }
+}
+
+const digitalPet = new Tamagotchi("Rex");
+// console.log(digitalPet)
+
+digitalPet.greet();
 
 // Do not edit below this line
 module.exports = Tamagotchi;
