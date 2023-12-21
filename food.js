c0a9d48f5af3cbf271cc38b094611a7dd8b4bd13
Jesse Chong <jessechong@pursuit.org>
2023-12-21 12:07:18 -0500

    Added BadFood that inherits from Food parent class

--- food.js
+++ food.js
@@ -5,9 +5,11 @@ class Food {
         this.daysToSpoil = daysToSpoil;
         this.fresh = fresh;
     }
+
     prepare() {
         console.log(`${this.name} is being prepared`)
     }
+
     isFresh() {
         if (this.daysToSpoil > 0) {
             console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
@@ -16,6 +18,7 @@ class Food {
             console.log(`${this.name} has spoiled.`);
         }
     }
+
         aDayPasses() {
             this.daysToSpoil -= 1;
             if (this.daysToSpoil <= 0) {
@@ -25,11 +28,73 @@ class Food {
         }
 }
 
-const pizzaBagel = new Food("pizza bagel", 3);
-pizzaBagel.prepare();
-pizzaBagel.isFresh();
-pizzaBagel.aDayPasses();
-pizzaBagel.aDayPasses();
-pizzaBagel.aDayPasses();
+class BadFood extends Food {
+    constructor(name, daysToSpoil = 20, fresh = true, weapons) {
+        super(name, daysToSpoil, fresh = true, weapons)
+        this.weapons = weapons;
+    }
+
+    prepare() {
+        console.log(`I am ${this.name} and my calories are too high to count!`);
+    }
+    
+    fight(opponent) {
+        const myWeapon = randomArrayItem(this.weapons);
+        opponent.daysToSpoil -= myWeapon.hitPoints;
+        console.log(`${opponent.name} is down to ${opponent.daysToSpoil} days, but I still have ${this.daysToSpoil} days left!`);
+        opponent.isFresh();
+        this.isFresh();
+      }
+    }
+    
+    function randomArrayItem(arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    }
+    
+    const donutWeapons = [
+      { name: "The Storm of 1000 Sprinkles!", hitPoints: 3 },
+      { name: "Hot Glaze Explosion!", hitPoints: 4}, 
+      { name: "Chocolate Ocean Tsunami!", hitPoints: 5}
+    ];
+    
+    const pizzaWeapons = [
+      { name: "Mouth Burn!", hitPoints: 3 },
+      { name: "Scorching Grease Blast!", hitPoints: 4 },
+      { name: "Jalapeno Tomato Sauce Rain!", hitPoints: 5 }  
+    ];
+    
+    const donut = new BadFood("Donut", 20, true, donutWeapons);
+    const pizza = new BadFood("Pizza", 20, true, pizzaWeapons);
+    
+    donut.prepare();
+    pizza.prepare();
+    
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+    donut.fight(pizza);
+    pizza.fight(donut);
+
+// const pizzaBagel = new Food("pizza bagel", 3);
+// pizzaBagel.prepare();
+// pizzaBagel.isFresh();
+// pizzaBagel.aDayPasses();
+// pizzaBagel.aDayPasses();
+// pizzaBagel.aDayPasses();
 // Do not edit below this line
 module.exports = Food;
