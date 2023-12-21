9dc558b4604143812b0ec2f39d7034a37aea47d2
marciafoster <mfoster9tre@gmail.com>
2023-12-21 12:34:50 -0500

    completed food class, added greet method to Tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,21 @@
 // Create class below
+class Tamagotchi{
+    constructor(name, energy, full, mood, sick, rehomed){
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.mood = mood;
+        this.sick = sick;
+        this.rehomed = rehomed
+    }
 
+    greet(){
+        console.log(`Hello, I'm ${this.name}.`)
+    }
+
+    status(){
+        
+    }
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
