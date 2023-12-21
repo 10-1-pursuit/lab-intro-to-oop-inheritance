720e861121a44e62ec7d0bfae40feb729b01bc05
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-21 13:59:37 -0500

    Finished Lab

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,69 +1,93 @@
 // Create class below
 // Create a virtual pet that can play, eat, sleep and more! Try to keep your pet happy or else it will be rehomed!
 
-class Pet {
-    constructor(name, energy, full, mood, sick, rehomed) {
+class Tamagotchi {
+    constructor(name, energy, full, mood, sick = false, rehomed = false) {
         this.name = name;
         this.energy = energy || 9;
         this.full = full || 8;
         this.mood = mood || 6;
-        this.sick = sick || false;
-        this.rehomed = rehomed || false;
+        this.sick = sick;
+        this.rehomed = rehomed;
     }
     greet() {
-        console.log(`Hello, I'm ${this.name}`)
+        return console.log(`Hello, I'm ${this.name}`)
     }
     status() {
-        console.log(`Energy: ${this.energy}, Hunger: ${this.full}, isSick: ${this.sick}, isRehomed:${this.rehomed}.`)
+        return console.log(`Energy: ${this.energy}, Hunger: ${this.full}, isSick: ${this.sick}, isRehomed:${this.rehomed}.`)
     }
-    eat(pet) {
+    eat() {
         this.full += 2
         this.energy -= 1
-        if (this.full >= 10) {
-            return pet.sick = true
+        if (this.full > 10) {
+            this.sick = true
         }
 
     }
-}
+    medicate() {
+        if (this.sick) {
+            this.full = 9
+            this.energy -= 3
+            this.sick = false
+        } else if (!this.sick) {
+            this.energy -= 1
+            return console.log("Tamagotchi refuses medicine")
+        }
+    }
+    play() {
+        if (this.sick === true) {
+            this.mood -= 1
+            this.energy -= 1
+        }
+        else if (this.sick === false && this.mood > 9) {
+            this.energy -= 2
+            this.full -= 1
+        } else if (this.energy <= 3) {
+            this.energy -= 1
+            return console.log("I am too tired to play")
+        } else {
+            this.mood += 2;
+            this.energy -= 1;
+            this.full -= 1;
+        }
 
-
-medicate() {
-    if (this.sick) {
-
+    }
+    sleep() {
+        this.energy += 4
+        this.full -= 3
+    }
+    timePasses() {
+        if (this.sick === false) {
+            this.mood -= 2
+            this.full -= 1
+            this.energy -= 1
+        }
+        if (this.sick === true) {
+            this.mood -= 3
+            this.full -= 2
+            this.energy -= 2
+        }
+    }
+    badGuardian() {
+        if (this.energy <= 0) {
+            this.rehomed = true
+        } else if (this.mood <= 0) {
+            this.rehomed = true
+        } else if (this.full <= 0) {
+            this.rehomed = true
+        }
+        return (`Your ${this.name} 's mood, full, and energy have been neglected, your ${this.name} has been rehomed!`)
     }
 }
 
-const mittens = new Pet("mittens", 10, 10, false, false)
+const mittens = new Tamagotchi("mittens", 10, 10, false, false)
+mittens.eat()
+mittens.play()
+mittens.sleep()
+mittens.medicate()
+mittens.timePasses()
 console.log(mittens.greet())
 console.log(mittens.status())
-// - Methods
 
-//   - greet: Tamagotchi console logs `Hello, I'm <name>!`
-//   - status: Tamogotchi console logs energy, full, mood, and sick values (see below for sample message)
-//   - eat: increases fullness by two, decreases energy by 1,
-//     - if eat method makes fullness more than 10, tamagotchi becomes sick (sick = true)
 
-// - medicate:
-//   - if tamagotchi is sick - set full to 9 - decrease energy by 3
-//   - if tamagotchi is not sick
-//     - console log refusal to take medicine
-//       - reduce energy by 1
-// - play: increases mood by 2, reduces energy and full by 1,
-//   - will not play if tamagotchi is sick,
-//   - if asked to play when sick, reduce mood and energy by 1
-//   - will not play if mood is above 9, reduce energy by 2 and full by 1
-//   - will not play if energy is less than or equal to 3
-//     - console log "I am too tired to play" - reduce energy by 1
-// - sleep:
-//   - increases energy by 4 and decreases full by 3
-// - timePasses:
-//   - if tamagotchi is not sick - mood decreases by 2, fullness and energy decrease by 1
-//     -if tamagotchi is sick - mood decreases by 3, fullness and energy decrease by 2
-// - badGuardian
-//   - logs a message that the tamagotchi has been rehomed
-//   - if energy is less than or equal to 0, set rehomed to true
-//   - if mood is less than or equal to 0 set rehomed to true
-//   - if full is less than or equal to true set rehomed to true
-
-// Do not edit below this line
 module.exports = Tamagotchi;
