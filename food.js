c45975384a6e6b33b80d3d8b2684d7206982ff81
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-12-21 11:41:50 -0500

    added the isFresh and aDayPasses methods to the food class

--- food.js
+++ food.js
@@ -1,21 +1,37 @@
 // Create class below
 class Food {
-  constructor(name, daysToSpoil, fresh = true) {
+  constructor(name, daysToSpoil = 0, fresh = true) {
     this.name = name;
     this.daysToSpoil = daysToSpoil;
     this.fresh = fresh;
   }
-  prepare(){
-    console.log(`${this.name} is being prepared`)
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    if (this.fresh === true && this.daysToSpoil > 0) {
+      console.log(
+        `There ${this.daysToSpoil === 1 ? "is" : "are"} ${
+          this.daysToSpoil
+        } day${this.daysToSpoil === 1 ? "" : "s"} left before ${
+          this.name
+        } spoils`
+      );
+    } else {
+      console.log(`The ${this.name} has spoiled`);
+    }
+  }
+  aDayPasses() {
+    this.daysToSpoil -= 1;
+    this.isFresh();
   }
 }
 
-const pasta = new Food("Alfredo", 5, true)
-console.log(pasta)
-pasta.prepare()
-
-
-
+const pasta = new Food("Alfredo", 5);
+// console.log(pasta)
+// pasta.prepare()
+// pasta.isFresh();
+pasta.aDayPasses();
 
 // Do not edit below this line
 module.exports = Food;
