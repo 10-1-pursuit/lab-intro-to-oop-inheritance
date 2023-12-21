4042df597f570c685b76900c8eab33f3276f3398
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-21 12:10:33 -0500

    Added class in the wrong js file

--- food.js
+++ food.js
@@ -1,4 +1,31 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil || 3;
+        this.fresh = fresh;
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared `)
+    }
+    isFresh() {
+        if (!fresh || daysToSpoil >= 3) {
+            console.log(`There are 3 days left before ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+
+    aDayPasses(foodItem) {
+        this.daysToSpoil -= 1
+        foodItem.isFresh()
+    }
+}
+
+const foodItem1 = new Food("yogurt", 5, true)
+
+
+
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,26 +1,5 @@
 // Create class below
-class Food {
-    constructor(name, daysToSpoil, fresh = true) {
-        this.name = name;
-        this.daysToSpoil = daysToSpoil || 3;
-        this.fresh = fresh;
-    }
-    prepare() {
-        console.log(`${this.name} is being prepared `)
-    }
-    isFresh() {
-        if (daysToSpoil >= 3) {
-            console.log(`There are 3 days left before ${this.name} spoils.`)
-        } else if (!fresh) {
-            console.log(`The ${this.name} is no longer fresh and will expire in ${this.daysToSpoil} days`)
-        }
-    }
 
-    aDayPasses(foodItem) {
-        this.daysToSpoil -= 1
-        foodItem.isFresh()
-    }
-}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
