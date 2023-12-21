9e281d78e31edb0eac65f9c39a70d50b2ad68809
Tyller Mendoza <tyllermendoza@gmail.com>
2023-12-21 01:27:15 -0500

    food class created

--- food.js
+++ food.js
@@ -1,4 +1,25 @@
 // Create class below
+class food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = fresh
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.daysToSpoil <= 0){
+            console.log(`${this.name} is spoiled`)
+        } else {
+            console.log(`There are ${this.daysToSpoil} days left befor ${this.name} spoils`)
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil -=1
+        this.isFresh()
+    }
+}
 
 // Do not edit below this line
 module.exports = Food;
